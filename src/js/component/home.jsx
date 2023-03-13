2d03c11a385010cdbf68b234cda5c4be703f5d8b
Sierra71999 <evans.m.sierra99@gmail.com>
2023-03-13 19:36:19 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  //if something was in the "" below it woul show up in text box on browser
+  const [inputValue, setInputValue] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const [showTrash, setShowTrash] = useState(false);
+
+  //add into array -> concat
+  //delete from array-> filter
+  //update -> map
+  // trash can fas fa-trash-alt
+  const url = "https://assets.breatheco.de/apis/fake/todos/user/sierra1999";
+  const getAllToDos = () => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => setToDos(data))
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    getAllToDos();
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>TO-DO LIST</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyPress={(e) => {
+              if (e.key == "Enter") {
+                console.log(e.key);
+                setToDos(toDos.concat(inputValue));
+                setInputValue("");
+                //the line above setInputValue(""); when u add something in the text and enter it erases it from the text bar
+              }
+            }}
+            placeholder="What do you need to do?"
+          ></input>
+        </li>
+        {toDos.map((item, index) => (
+          <li
+            className="d-flex"
+            onMouseEnter={() => setShowTrash(true)}
+            onMouseLeave={() => setShowTrash(false)}
+          >
+            {item.label}
+            <i
+              className={
+                showTrash
+                  ? "fas fa-trash-alt d-block"
+                  : "fas fa-trash-alt d-none"
+              }
+              onClick={() =>
+                setToDos(
+                  toDos.filter((t, currentIndex) => index != currentIndex)
+                )
+              }
+            ></i>
+          </li>
+        ))}
+        {/*the {item} before i added it when i hit enter the words i types would not appear the only thing there was the icon  */}
+      </ul>
+      <div>{toDos.length}</div>
+    </div>
+  );
 };
 
 export default Home;
